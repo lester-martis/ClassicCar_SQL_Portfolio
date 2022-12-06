c7955d181e9c6117a1f08f697484a1c5bc1541da
Lester Martis <69054365+lester-martis@users.noreply.github.com>
2022-12-06 21:09:33 +0400

    Add files via upload

--- classiccars.sql
+++ classiccars.sql
@@ -4,7 +4,7 @@
 				This project is for Explanatory Data Analysis using DML.
 Link to the Dataset: https://github.com/agatakj/MySLQ/blob/master/classicmodels.sql
 
-				 */
+*/
 
 /* 1. Retrieve the data for Total Order amount for Each customer and present Top 10 customers */ 
 SELECT
@@ -57,4 +57,65 @@ JOIN
 GROUP BY cs.customerName, pd.productLine
 ORDER BY cs.customerName;
 
+/* 4. Yearly Sales and Sold Qty */
+SELECT
+	year(od.orderDate) As Years,
+    SUM(os.quantityOrdered) AS Sold_Qty,
+    SUM(os.quantityOrdered * os.priceEach) AS Total_Sale
+FROM
+	orders od
+JOIN
+	orderdetails os ON od.orderNumber = os.orderNumber
+GROUP BY year(od.orderDate);
+
+
+/* 5. Yearly Profit Summary */
+SELECT
+	YEAR(od.orderDate) AS Years,
+    SUM(os.priceEach * os.quantityOrdered) AS Revenue,
+	SUM(os.priceEach - pd.buyPrice) * os.quantityOrdered AS Gross_Profit,
+    ROUND (SUM(os.priceEach - pd.buyPrice) * os.quantityOrdered / SUM(os.priceEach * os.quantityOrdered) * 100, 2) AS GP_Per
+FROM
+	products pd
+JOIN
+	orderdetails os ON pd.productCode = os.productCode
+JOIN
+	orders od ON os.orderNumber = od.orderNumber
+GROUP BY YEAR(od.orderDate) ;
+
+
+/* 6. Product Lines GP% for the years 2003-2005(May) */
+SELECT
+	YEAR(od.orderDate) AS Years,
+    pd.productLine,
+    SUM(os.quantityOrdered * os.priceEach) AS Revenue,
+    SUM(os.priceEach - pd.buyPrice) * os.quantityOrdered  AS Gross_Profit,
+   ROUND(SUM(os.priceEach - pd.buyPrice) * os.quantityOrdered / SUM(os.quantityOrdered * os.priceEach) * 100,2) AS GP_Perc
+FROM
+	orders od
+JOIN
+	orderdetails os ON od.orderNumber = os.orderNumber
+JOIN
+	products pd ON os.productCode = pd.productCode
+GROUP BY YEAR(od.orderDate), pd.productLine;
+
+/* 7. Highest Gross Profit by customers */ 
+
+SELECT
+	YEAR(od.orderDate) AS Years,
+    cs.customerName,
+    SUM(os.quantityOrdered * os.priceEach) - SUM(pd.buyPrice * os.quantityOrdered) AS GP_Amount,
+    ROUND(SUM(pd.buyPrice * os.quantityOrdered) / SUM(os.quantityOrdered * os.priceEach) * 100,2) AS GP_Perc
+FROM
+	customers cs
+INNER JOIN 
+	orders od ON cs.customerNumber = od.customerNumber
+INNER JOIN
+	orderdetails os ON od.orderNumber = os.orderNumber
+INNER JOIN
+	products pd ON os.productCode = pd.productCode
+WHERE status = 'Shipped'
+GROUP BY customerName;
 	
+    
+    
